06d729ddd4b7e7afa9fa716448771e38199ae4e5
J. Rosenbaum <minxdragon@gmail.com>
2022-10-17 16:25:31 +1100

    oops removed extra camera!

--- streamlit_test.py
+++ streamlit_test.py
@@ -22,11 +22,10 @@ if __name__ == '__main__':
     args = parser.parse_args()
     
     uploaded_target_file = st.camera_input("Take a picture")
-    img_file_buffer = st.camera_input("Take a picture")
 
-    if img_file_buffer is not None:
+    if uploaded_target_file is not None:
         # To read image file buffer as a PIL Image:
-        img = Image.open(img_file_buffer)
+        img = Image.open(uploaded_target_file)
 
         # To convert PIL Image to numpy array:
         img_array = np.array(img)
