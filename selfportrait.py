273452e901f5881476ad556c6bb53a6f15e74e83
J. Rosenbaum <minxdragon@gmail.com>
2023-01-20 14:49:55 +1100

    self portrait working again

--- selfportrait.py
+++ selfportrait.py
@@ -159,7 +159,6 @@ def selfPortrait():
     facefile = ()
 
     userSelected = None #convert array to string
-    filename = 'https://thispersondoesnotexist.com/image'
 
     promptString = "a head and shoulders portrait of a person, full face, with a neutral expression of a person who is " + analysisComplete + " painted by a portrait artist"
 
@@ -180,13 +179,13 @@ def selfPortrait():
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
 
-                _, frame, dst_img = self.video.read()
+                _, dst_img = self.video.read()
                 dst_points, dst_shape, dst_face = select_face(dst_img, choose=False)
                 if dst_points is not None:
                     dst_img = face_swap(self.src_face, dst_face, self.src_points, dst_points, dst_shape, dst_img, self.args, 68)
                 self.writer.write(dst_img)
                 # Send the frame to Syphon
-                client.publish(frame)
+                # client.publish(frame)
                 #if self.args.show:
                 cv2.imshow("Video", dst_img)
 
