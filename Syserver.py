11fb4c6c1773fe01aee3c73c787ca6af136e6357
J. Rosenbaum <minxdragon@gmail.com>
2023-01-25 14:34:24 +1100

    now trying integration to the syserver

--- Syserver.py
+++ Syserver.py
@@ -90,26 +90,55 @@ def main():
     # loop
     # while not server1.should_close() and not server2.should_close():
     while not server2.should_close():
-        ret, frame = cap.read() #read camera image
-        frame = cv2.resize(frame, size)
-        # frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) #BGR --> RGB
-        # frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #BGR --> GRAY
-        # frame_gray = cv2.cvtColor(frame_gray, cv2.COLOR_GRAY2RGB) # GRAY (3 channels)
-        VideoHandler(args.video_path, args.src_img, args).start()
-        
-        #cv2.imshow("rgb", frame)
-        #server1.draw_and_send(frame_rgb) # Syphon.Server.draw_and_send(frame) draw frame using opengl and send it to syphon
-        
-        # cv2.imshow("python", dst_img)
-        # server2.draw_and_send(dst_img)
-        resized = cv2.resize(self.dst_img, (640, 400))
-        print ('showing video')
-        cv2.imshow("python", resized)
-        print ('sending video')
-        server2.draw_and_send(resized)
-            
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        class VideoHandler(object):
+            def __init__(self, video_path=0, img_path=None):
+                self.src_points, self.src_shape, self.src_face = select_face(cv2.imread(img_path))
+                if self.src_points is None:
+                    print('No face detected in the source image !!!')
+                    exit(-1)
+                self.args = args
+                self.video = cv2.VideoCapture(video_path)
+
+                cap = cv2.VideoCapture(0)
+
+                while True:
+                    _, frame = cap.read()
+                    dst_points, dst_shape, dst_face = select_face(frame, choose=False)
+                    if dst_points is not None:
+                        frame = face_swap(self.src_face, dst_face, self.src_points, dst_points, dst_shape, frame, args, 68)
+                    
+                    resized = cv2.resize(frame, (640, 400))
+                    cv2.imshow("Video", resized)
+                    if cv2.waitKey(1) & 0xFF == ord('q'):
+                        break
+
+                cap.release()
+                cv2.destroyAllWindows()
+
+                # 
+                # cv2.imshow("Video", resized)
+
+
+        if __name__ == '__main__':
+            logging.basicConfig(level=logging.INFO,
+                                format="%(levelname)s:%(lineno)d:%(message)s")
+
+            parser = argparse.ArgumentParser(description='FaceSwap Video')
+            parser.add_argument('--src_img', required=False, default='interactive/data/dream.jpg',
+                                help='Path for source image')
+            parser.add_argument('--video_path', default=0,
+                                help='Path for video')
+            parser.add_argument('--warp_2d', default=False, action='store_true', help='2d or 3d warp')
+            parser.add_argument('--correct_color', default=False, action='store_true', help='Correct color')
+            parser.add_argument('--show', default=False, action='store_true', help='Show')
+            parser.add_argument('--save_path', required=False, default="test/test.avi", help='Path for storing output video')
+            args = parser.parse_args()
+
+            dir_path = os.path.dirname(args.save_path)
+            if not os.path.isdir(dir_path):
+                os.makedirs(dir_path)
+
+            VideoHandler(video_path=0, img_path='interactive/data/dream.jpg')
 
     glfw.terminate()
     cv2.destroyAllWindows()
--- quickvideo.py
+++ quickvideo.py
@@ -8,7 +8,7 @@ from face_swap import face_swap
 
 
 class VideoHandler(object):
-    def __init__(self, video_path=0, img_path=None, args=None):
+    def __init__(self, video_path=0, img_path=None):
         self.src_points, self.src_shape, self.src_face = select_face(cv2.imread(img_path))
         if self.src_points is None:
             print('No face detected in the source image !!!')
@@ -55,4 +55,4 @@ if __name__ == '__main__':
     if not os.path.isdir(dir_path):
         os.makedirs(dir_path)
 
-    VideoHandler(args.video_path, args.src_img, args)
+    VideoHandler(video_path=0, img_path='interactive/data/dream.jpg')
