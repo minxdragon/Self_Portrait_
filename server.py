1ea7a5af4b742e24af9cc7f0e68e4b6f52693555
J. Rosenbaum <minxdragon@gmail.com>
2023-01-26 18:40:26 +1100

    now running inside the main code! (but no end)

--- server.py
+++ server.py
@@ -45,7 +45,8 @@ import Syphon
 from Syphon import Server
 from urllib.request import urlopen, Request
 from face_detection import select_face
-from face_swap import face_swap
+import sySwap
+from sySwap import VideoHandler
 from Syserver import main
 #from grayServer import main
 #from syphonpy import Server
@@ -168,8 +169,31 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 				print(f'Sending...')                
 				conn.sendall(b"cameraMaskReady,window frame name")
 				print(f'Analysis complete. Mask and Keywords sent.')
-				
-				
+				# run videoHandler from the syswap file
+				if __name__ == '__main__':
+					print('starting sySwap.py')
+					logging.basicConfig(level=logging.INFO,
+										format="%(levelname)s:%(lineno)d:%(message)s")
+
+					parser = argparse.ArgumentParser(description='FaceSwap Video')
+					parser.add_argument('--src_img', required=False, default='interactive/data/dream.jpg',
+										help='Path for source image')
+					parser.add_argument('--video_path', default=0,
+										help='Path for video')
+					parser.add_argument('--warp_2d', default=False, action='store_true', help='2d or 3d warp')
+					parser.add_argument('--correct_color', default=False, action='store_true', help='Correct color')
+					parser.add_argument('--show', default=False, action='store_true', help='Show')
+					parser.add_argument('--save_path', required=False, default="test/test.avi", help='Path for storing output video')
+					args = parser.parse_args()
+
+					dir_path = os.path.dirname(args.save_path)
+					if not os.path.isdir(dir_path):
+						os.makedirs(dir_path)
+
+					VideoHandler(video_path=0, img_path='interactive/data/dream.jpg', args=args).start()
+
+
+
 			# 	counter = 0
 			# 	while True:
 			# 		try:
