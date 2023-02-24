8b3bea9a70b402f32c85d40193d942a1e3d4ad33
J. Rosenbaum <minxdragon@gmail.com>
2023-02-24 13:13:36 +1100

    added automation for gallery

--- processing.sh
+++ processing.sh
@@ -4,9 +4,26 @@ export PATH="/Applications/Processing.app/Contents/MacOS:$PATH"
 
 #open processing windows
 #cd ~/Documents/GitHub/FaceSwap
-cd /Users/sgm_tech/Documents/Self_Portrait_-base 
-# /Applications/Processing.app/Contents/MacOS/processing syphoncam/syphoncam.pde --present
-# /Applications/Processing.app/Contents/MacOS/processing interactive/interactive.pde --present
-open syphoncam/macos-x86_64/syphoncam.app
-sleep 5
+
+#open Syphoncam
+cd  /Users/sgm_tech/Documents/Self_Portrait_-base/syphoncam 
+#cd ~/Documents/GitHub/FaceSwap/syphoncam
+nohup processing-java --sketch=$PWD --run &
+echo "Syphoncam is running"
+
+sleep 2
+
+cd /Users/sgm_tech/Documents/Self_Portrait_-base/interactive/data/galleryPlayer
+#cd ~/Documents/GitHub/FaceSwap/interactive/data/galleryPlayer
+nohup processing-java --sketch=$PWD --run &
+echo "galleryPlayer is running"
+
+sleep 2
+
+cd /Users/sgm_tech/Documents/Self_Portrait_-base/interactive/galleryPlayer2
+#cd ~/Documents/GitHub/FaceSwap/interactive/data/galleryPlayer2
+nohup processing-java --sketch=$PWD --run &
+echo "galleryPlayer2 is running"
 #open interactive/macos-x86_64/interactive.app
+
+echo "to check these processes, type bg or check the nohup.out file"
